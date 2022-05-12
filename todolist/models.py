b39f59d54ce1c8936e90d834aa3d91609ce14925
MikesoWeb <klionpsyx@gmail.com>
2022-05-12 14:12:32 +0300

    'edit_models'

--- todolist/models.py
+++ todolist/models.py
@@ -3,8 +3,12 @@ from django.db import models
 
 # Create your models here.
 class ToDo(models.Model):
-    title = models.CharField(max_length=350)
-    is_complete = models.BooleanField(default=False)
+    title = models.CharField('Название задания', max_length=500)
+    is_complete = models.BooleanField('Завершено', default=False)
+
+    class Meta:
+        verbose_name = 'Задание'
+        verbose_name_plural = 'Задания'
 
     def __str__(self):
         return self.title
