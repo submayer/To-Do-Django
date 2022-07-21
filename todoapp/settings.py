39ff042c552e767ed2dc8440d6b8d589b6c541df
Михаил Терехов <47977649+MikesoWeb@users.noreply.github.com>
2022-07-21 09:59:32 +0300

    Update settings.py

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-z+h#pachd*bo#!_a@=o)imrbwx-8vj10)mlm*6qbg%k#ids^df
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = ['*']
 
 # Application definition
