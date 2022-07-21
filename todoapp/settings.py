738d6f246fa3e914ad906eba6ea4d18f3e9ea0ed
Михаил Терехов <47977649+MikesoWeb@users.noreply.github.com>
2022-07-21 09:51:34 +0300

    Update settings.py

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z+h#pachd*bo#!_a@=o)imrbwx-8vj10)mlm*6qbg%k#ids^df'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '*',
